5809a18b84df9f64cc842125439353491ae0baeb
Erix Rivas <erixrivas@gmail.com>
2021-11-04 23:38:50 -0500

    añadiendo variable de entorno

--- proyecto/proyecto/settings.py
+++ proyecto/proyecto/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zfu&#&)dbp0&(&h-o#kf+!ew*l9)*xpyb)*20+r_&^o=!fzci*'
+from dotenv import load_dotenv
+load_dotenv()
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
